a3bc88a3325fbaa328663033e3ae098f344ec7e5
Christian Aschoff <christian.aschoff@diw-sc.de>
2020-03-22 10:05:56 +0100

    add initial component for trader

--- src/app/trader/trader.module.ts
+++ src/app/trader/trader.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrdersComponent } from './orders/orders.component';
 import { CreateProductComponent } from './create-product/create-product.component';
+import { TraderMainComponent } from './trader-main/trader-main.component';
 
 
 
 @NgModule({
   declarations: [
     OrdersComponent,
-    CreateProductComponent
+    CreateProductComponent,
+    TraderMainComponent
   ],
   imports: [
     CommonModule
