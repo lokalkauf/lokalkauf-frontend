be3948263c781a722425ba7fd98e6cfbac930fb7
TheCodemonkey <7ilya@gmx.de>
2020-03-22 13:15:53 +0100

    custom inputs

---
+++ src/app/reusables/input-fld/input-fld.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-input-fld',
+  templateUrl: './input-fld.component.html',
+  styleUrls: ['./input-fld.component.scss']
+})
+export class InputFldComponent implements OnInit {
+
+  @Input() value:string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
