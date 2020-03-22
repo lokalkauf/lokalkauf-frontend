93818c6e7b402342a51e414a0888d82ae4d4a7d0
Christian Aschoff <christian.aschoff@diw-sc.de>
2020-03-22 15:08:30 +0100

    make product view fancy

--- src/app/customer/product-item/product-item.component.ts
+++ src/app/customer/product-item/product-item.component.ts
@@ -3,20 +3,7 @@ import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-product-item',
-  template: `
-      <a [routerLink]="'/product-detail/' + product.id">
-        <mat-card class="product-item">
-          <div class="image-wrapper" mat-card-image [ngStyle]="{'background-image': 'url(' + product.image + ')'}">
-          </div>
-          <mat-card-content>
-            <p class="title">{{ product.name }}</p>
-            <p class="price">
-              {{ product.price }} €
-            </p>
-          </mat-card-content>
-        </mat-card>
-      </a>
-  `,
+  templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.scss']
 })
 export class ProductItemComponent implements OnInit {
