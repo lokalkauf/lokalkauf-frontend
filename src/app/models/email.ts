3b5460d62a3f2c9a3b50c714a25f4b1a30970d3a
Kai Nitsch <kainitsch55@gmail.com>
2020-03-26 18:50:20 +0100

    define new email component / form for app

---
+++ src/app/models/email.ts
@@ -0,0 +1,6 @@
+export class EMail {
+  id: number;
+  from_name: string;
+  from_emailOrPhone: string;
+  message: string;
+}
---
+++ src/app/services/email.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { AppPage } from '../app.po';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { EMail } from './mail-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EMailService {
+
+  ServerUrl = 'http://localhost/dev/blogger/'; // TODO define backend url
+  errorData: {};
+
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
+
+  constructor(private http: HttpClient) { }
+
+  getPage(slug: string) {
+    return this.http.get<AppPage>(this.ServerUrl + 'api/page/' + slug)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  contactForm(formdata: EMail) {
+    // TODO contact backend for mail sending here
+    // return this.http.post<EMail>(this.ServerUrl + 'api/contact', formdata, this.httpOptions).pipe(
+    //   catchError(this.handleError)
+    // );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+
+      // A client-side or network error occurred. Handle it accordingly.
+
+      console.error('An error occurred:', error.error.message);
+    } else {
+
+      // The backend returned an unsuccessful response code.
+
+      // The response body may contain clues as to what went wrong.
+
+      console.error(`Backend returned code ${error.status}, ` + `body was: ${error.error}`);
+    }
+
+    // return an observable with a user-facing error message
+
+    this.errorData = {
+      errorTitle: 'Oops! Request for document failed',
+      errorDesc: 'Something bad happened. Please try again later.'
+    };
+    return throwError(this.errorData);
+  }
+}
