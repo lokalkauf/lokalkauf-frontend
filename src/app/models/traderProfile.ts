175c08dfaf4c2686ac2385fec02b9632d156b3aa
Patrick Simmelbauer <p.simmelbauer@salt-and-pepper.eu>
2020-03-27 22:24:06 +0100

    add more fields to registration

---
+++ src/app/models/traderProfile.ts
@@ -0,0 +1,9 @@
+export interface TraderProfile {
+  businessname: string;
+  ownerFirstname: string;
+  ownerLastname: string;
+  postcode: string;
+  city: string;
+  street: string;
+  number: number;
+}
