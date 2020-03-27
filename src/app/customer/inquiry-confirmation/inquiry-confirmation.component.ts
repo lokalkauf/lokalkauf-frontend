20c515f8da496d47591c5f5674d7338848ea02a8
darkblackside <merschibjoern@aol.de>
2020-03-27 20:38:30 +0100

    Added inquiry confirmation page

---
+++ src/app/customer/inquiry-confirmation/inquiry-confirmation.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-inquiry-confirmation',
+  templateUrl: './inquiry-confirmation.component.html',
+  styleUrls: ['./inquiry-confirmation.component.scss'],
+})
+export class InquiryConfirmationComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+
+  redirectToStart() {
+    this.router.navigate(['/localtraders']);
+  }
+}
