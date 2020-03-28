174d21743af8809a2879b6de3addc58ca7d45c0f
Kai Nitsch <kainitsch55@gmail.com>
2020-03-28 15:47:56 +0100

    add data for contact form

--- src/app/customer/trader-contact/trader-contact.component.ts
+++ src/app/customer/trader-contact/trader-contact.component.ts
@@ -1,9 +1,14 @@
 import {Component, Input, OnInit} from '@angular/core';
-import { Router } from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 import { Trader } from '../../models/trader';
 import { EMail } from '../../models/email';
-import {EMailService} from "../../services/email.service";
+import {EMailService} from '../../services/email.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {TraderProfile} from '../../models/traderProfile';
+import {Observable} from 'rxjs';
+import {flatMap, map} from 'rxjs/operators';
+import {AngularFirestore} from '@angular/fire/firestore';
 
 
 @Component({
@@ -20,20 +25,103 @@ export class TraderContactComponent implements OnInit {
   submitted = false;
   error: {};
 
-  constructor(private router: Router,
-              private mailService: EMailService) { }
+  traderPhone: string;
+  traderMail: string;
 
-  ngOnInit(): void {
+
+  contactForm = new FormGroup(
+    {
+      mail_message: new FormControl('', [Validators.required]),
+      mail_contact: new FormControl('', [Validators.required]),
+      agbRead: new FormControl('', [Validators.requiredTrue]),
+    },
+  );
+
+  get mail_contact() {
+    return this.contactForm.get('mail_contact');
   }
 
-  onSubmit() {
+  get mail_message() {
+    return this.contactForm.get('mail_message');
+  }
+
+  get agbRead() {
+    return this.contactForm.get('agbRead');
+  }
+
+  constructor(private router: Router,
+              private mailService: EMailService,
+              private db: AngularFirestore,
+              private route: ActivatedRoute) { }
+
+
+  ngOnInit(): void {
+    let traderId;
+    this.route.params.subscribe(p => {
+      traderId = p.traderId;
+    });
+
+    const docReference = this.db.collection('Traders').doc(traderId);
+    docReference.get().subscribe(p => {
+      if (p.exists) {
+        const traderData = p.data();
+        this.traderPhone =  traderData.telephone;
+        this.traderMail = traderData.email;
+      } else {
+        // doc.data() will be undefined in this case
+        console.log('No such trader!');
+      }
+    });
+
+  }
+
+  async onSubmit() {
     this.submitted = true;
     // TODO finalize call for backend sending mail
-    // return this.mailService.contactForm(this.mailModel)
-    //   .send(
-    //   data => this.mailModel = data,
-    //   error => this.error = error
-    // );
+
+    const email: EMail = {
+      acceptedAgb: false,
+      fromEMailOrPhone: '',
+      fromName: '',
+      id: 0,
+      message: '',
+      title: '',
+      toEMail: '',
+      toName: ''
+    };
+
+    try {
+      await this.mailService.send(email);
+    } catch (e) {
+      // TODO show ERRORs
+      // switch (e.code) {
+      //   case 'auth/email-already-in-use':
+      //     this.registrationForm.setErrors({
+      //       emailInUse: true,
+      //     });
+      //     break;
+      //   case 'auth/invalid-email':
+      //     this.registrationForm.setErrors({
+      //       invalidEmail: true,
+      //     });
+      //     break;
+      //   case 'auth/operation-not-allowed':
+      //     this.registrationForm.setErrors({
+      //       undefinedError: true,
+      //     });
+      //     break;
+      //   case 'auth/weak-password':
+      //     this.registrationForm.setErrors({
+      //       weakPassword: true,
+      //     });
+      //     break;
+      //   default:
+      //     this.registrationForm.setErrors({
+      //       undefinedError: true,
+      //     });
+      //     break;
+      // }
+    }
   }
 
   gotoHome() {
--- src/app/models/trader.ts
+++ src/app/models/trader.ts
@@ -2,5 +2,7 @@ export interface Trader {
   id: string;
   name: string;
   image: string;
+  email: string;
+  phone: string;
   products: string[];
 }
