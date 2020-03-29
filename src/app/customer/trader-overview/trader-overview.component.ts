a785edbfc02f981ef40e2c269547ee60254324ef
TheCodemonkey <7ilya@gmx.de>
2020-03-29 16:15:35 +0200

    fixe for 10 items in where query limitation

--- src/app/customer/trader-overview/trader-overview.component.ts
+++ src/app/customer/trader-overview/trader-overview.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest, forkJoin } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Trader } from '../../models/trader';
 import { Location } from 'src/app/models/location';
@@ -15,19 +16,14 @@ import { TraderProfile } from 'src/app/models/traderProfile';
   styleUrls: ['./trader-overview.component.scss'],
 })
 export class TraderOverviewComponent implements OnInit {
-  traders$: Observable<TraderProfile[]>;
+  traders$: Array<TraderProfile> = new Array<TraderProfile>();
 
   constructor(
     db: AngularFirestore,
     private route: ActivatedRoute,
     private geo: GeoService,
     private traderService: TraderService
-  ) {
-    // this.traders$ = this.traderService.getTraderProfiles([
-    //   '14zQgp6I6afO54FkUUY54CtNYd92',
-    //   'l1ByOLFIvaPlm8JsqLetNWHlCcA2',
-    // ]);
-  }
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
@@ -48,14 +44,51 @@ export class TraderOverviewComponent implements OnInit {
   updateLocations(trlocaitons: Array<Location>) {
     console.log('UÖDATESASDFADF');
 
-    let ids = trlocaitons.map((l) => l.traderId);
 
-    if (ids.length > 10) {
-      ids = ids.slice(0, 10);
+    // a lot of magic, couse of firebase limitation loading 10 ids in query at once
+    const ids = trlocaitons.map((l) => l.traderId);
+    const chunked = this.getChunks(ids, 10);
+    console.log(chunked);
+
+
+    for (const chunk of chunked) {
+      this.traderService
+        .getTraderProfiles(chunk)
+        .subscribe((t: TraderProfile[]) => {
+          console.log('loading of trader done.');
+          if (t && t.length > 0) {
+            console.log('trader loaded: ' + t.length);
+
+            t.forEach((trader) => {
+              this.traders$.push(trader);
+            });
+          }
+        });
     }
 
-    console.log(ids);
+    // forkJoin(() => {
+    //   const arr: Array<Observable<TraderProfile[]>> = new Array();
 
-    this.traders$ = this.traderService.getTraderProfiles(ids);
+    //   for (let x = 0; x < chunked; x++) {
+    //     arr.push(this.traderService.getTraderProfiles(chunked[x]));
+    //   }
+
+    //   return arr;
+    // }).subscribe((s) => {
+    //   console.log('chunks loaded...');
+    //   console.log(s);
+    // });
+
+    // this.traders$ = this.traderService.getTraderProfiles(ids);
+  }
+
+  getChunks(arr, size) {
+    return arr.reduce((acc, _, i) => {
+      if (i % size === 0) {
+        acc.push(arr.slice(i, i + size));
+      }
+
+      return acc;
+    }, []);
   }
 }
