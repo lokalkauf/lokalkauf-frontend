95473c649cb69cab29ef9f8702284e96192a94f3
Konstantin <buecheko@protonmail.com>
2020-03-29 18:57:05 +0200

    Added function to check if number of files is exceeded

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,8 @@ import * as admin from 'firebase-admin';
 import * as nodemailer from 'nodemailer';
 admin.initializeApp();
 
+const MAX_NUMBER_OF_IMAGES = 5;
+
 /**
  * Here we're using Gmail to send
  */
@@ -40,3 +42,31 @@ export const sendMail = functions.https.onCall((data, context) => {
   });
   return;
 });
+
+export const checkFileNumberLimit = functions.storage
+  .object()
+  .onFinalize(async (object) => {
+    if (object.name !== undefined) {
+      const filePath = object.name;
+      const bucket = admin.storage().bucket();
+      const directory = filePath.split('/').slice(0, 2).join('/');
+
+      bucket
+        .getFiles({ directory: directory })
+        .then(function (files) {
+          if (files[0].length > MAX_NUMBER_OF_IMAGES) {
+            console.log('Reachd max file num. Delete File...');
+            bucket
+              .file(filePath)
+              .delete()
+              .catch(
+                () =>
+                  function () {
+                    console.log('File delete faild');
+                  }
+              );
+          }
+        })
+        .catch(() => console.log('Check faild'));
+    }
+  });
