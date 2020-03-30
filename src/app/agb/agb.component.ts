e3c87d09dfb0d33555268f57a763956e2c338009
darkblackside <merschibjoern@aol.de>
2020-03-30 13:22:15 +0200

    Removed AGB and Imprint, linked the AGBs and Imprint and data protection pages to https://lokalkauf.org

--- src/app/agb/agb.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-agb',
-  templateUrl: './agb.component.html',
-  styleUrls: ['./agb.component.scss'],
-})
-export class AgbComponent {}
