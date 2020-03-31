ca8942c742d5e7f1fa8f50967b6f8b12c7b82020
Kai Nitsch <kainitsch55@gmail.com>
2020-03-31 20:19:40 +0200

    defined basic rating-star component

---
+++ src/app/reusables/rating-star/rating-star.component.ts
@@ -0,0 +1,56 @@
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  ViewEncapsulation,
+} from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-rating-star',
+  templateUrl: './rating-star.component.html',
+  styleUrls: ['./rating-star.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
+})
+export class RatingStarComponent implements OnInit {
+  @Input() private rating: number;
+  @Input() private starCount: number;
+  @Input() private color: string;
+  @Output() private ratingUpdated = new EventEmitter();
+
+  private snackBarDuration = 2000;
+  private ratingArr = [];
+
+  constructor(private snackBar: MatSnackBar) {}
+
+  ngOnInit() {
+    console.log('a ' + this.starCount);
+    for (let index = 0; index < this.starCount; index++) {
+      this.ratingArr.push(index);
+    }
+  }
+  onClick(rating: number) {
+    console.log(rating);
+    this.snackBar.open('You rated ' + rating + ' / ' + this.starCount, '', {
+      duration: this.snackBarDuration,
+    });
+    this.ratingUpdated.emit(rating);
+    return false;
+  }
+
+  showIcon(index: number) {
+    if (this.rating >= index + 1) {
+      return 'star';
+    } else {
+      return 'star_border';
+    }
+  }
+}
+
+export enum StarRatingColor {
+  primary = 'primary',
+  accent = 'accent',
+  warn = 'warn',
+}
--- src/app/reusables/reusables.module.ts
+++ src/app/reusables/reusables.module.ts
@@ -14,6 +14,10 @@ import { LkPageTitleComponent } from './lk-page-title/lk-page-title.component';
 import { LkCheckboxComponent } from './lk-checkbox/lk-checkbox.component';
 import { ImageChooserComponent } from './image-chooser/image-chooser.component';
 import { FullscreenModalComponent } from './fullscreen-modal/fullscreen-modal.component';
+import { RatingStarComponent } from './rating-star/rating-star.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 const routes: Routes = [];
 
@@ -29,6 +33,7 @@ const routes: Routes = [];
     LkCheckboxComponent,
     ImageChooserComponent,
     FullscreenModalComponent,
+    RatingStarComponent,
   ],
   imports: [
     CommonModule,
@@ -37,6 +42,9 @@ const routes: Routes = [];
     LeafletModule.forRoot(),
     RouterModule.forRoot(routes),
     MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTooltipModule,
   ],
   exports: [
     MapComponent,
