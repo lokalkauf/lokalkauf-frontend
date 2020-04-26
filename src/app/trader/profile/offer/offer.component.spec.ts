c2ef7fdd8d29d83db4aafa663178190da528548b
Patrick Simmelbauer <p.simmelbauer@salt-and-pepper.eu>
2020-04-26 13:46:40 +0200

    add separate tab for image upload

---
+++ src/app/trader/profile/offer/offer.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OfferComponent } from './offer.component';
+
+describe('OfferComponent', () => {
+  let component: OfferComponent;
+  let fixture: ComponentFixture<OfferComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [OfferComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OfferComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
