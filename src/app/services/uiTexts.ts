6c8fe5609bfaf4a1b35b77a991421efeef9789ef
Christian Aschoff <christian.aschoff@diw-sc.de>
2020-07-11 15:13:23 +0200

    text changes

--- src/app/services/uiTexts.ts
+++ src/app/services/uiTexts.ts
@@ -11,24 +11,23 @@ export const uiTexts = {
   umkreissuche_userlocation_text: 'Deinen Wohnort finden / Postleitzahlsuche',
   umkreissuche_userlocation_gpsenabled: ' (mein Standort)',
   start_button_geschaeft_suchen: 'Geschäft finden',
-  start_zusammen_headline: 'Wir bringen Euch wieder zusammen!',
+  start_zusammen_headline: 'Buy local - Act global',
   start_zusammen_text:
-    'Du möchtest lieber bei Deinem Lieblingsladen oder Lieblingsrestaurant um die Ecke ' +
-    'bestellen oder Dich beliefern lassen und damit lokale Geschäfte unterstützen?' +
+    'Finde Deine Lieblingsprodukte direkt bei Dir in der Nähe und unterstütze beim Kauf nicht nur ' +
+    'den Händler und Gastronom vor Ort, sondern deine ganze Community.' +
     '<br /><br />Du bist ein lokaler Einzelhändler oder Gastronom und möchtest online mehr Kunden erreichen?' +
-    '<br />Wir helfen Euch!',
+    '<br />Wir helfen Dir!',
   start_button_haender_anmelden: 'Geschäftsprofil anlegen',
   start_button_haender_bearbeiten: 'Geschäftsprofil bearbeiten',
   start_mission_headline: 'Unsere Mission',
   start_mission_text:
-    'Wir vermissen es so sehr, zusammen essen zu gehen, zu reden sowie zu lachen und dann ab in die nächste ' +
-    'Bar auf einen Drink weiterzuziehen. Wir vermissen das entspannte Bummeln in der Stadt und das Stück Kuchen ' +
-    'mit einem Kaffee in unserem Lieblings-Café.' +
-    '<br /><br />Wir wollen, dass es unsere Lieblingsläden, Lieblingsrestaurants und das Geschäft um die Ecke auch ' +
-    'nach der Coronakrise noch gibt. Daher unterstützt uns, um lokal zu unterstützen.' +
-    '<br />lokalkauf – kauft lokal' +
-    '<br /><br />Wir wollen eine kostenlose Plattform schaffen, die den lokalen Einzelhandel und die Gastronomie ' +
-    'mit den Bürgern digital verbindet und dabei nachhaltig unterstützt, die Coronakrise zu bewältigen',
+    'Wir glauben, dass der lokale Handel globale Auswirkungen hat.Wir glauben, dass jedes Geschäft um die Ecke ' +
+    'wichtig für unser Leben, für unsere lokale Gemeinschaft und für unsere globale Gesellschaft ist.' +
+    '<br /><br />Wir haben eine einfach zu bedienende und kostenlose Plattform geschaffen, die den lokalen Einzelhandel ' +
+    'digital mit den Bürgern verbindet und die lokale Region nachhaltig unterstützt, indem sie ein Bewusstsein für den ' +
+    'lokalen Einzelhandel und die Gastronomie schafft.' +
+    '<br /><br />Wir sind das digitale Schaufenster des lokalen, stationären Einzelhandels, der Restaurants und der Geschäfte ' +
+    'um die Ecke.Als Team, als Plattform und als Verein engagieren wir uns für den lokalen Einzelhandel.',
   start_mission_aboutus: 'Mehr über uns erfahren',
   start_wirvsvirus_headline: 'Im offiziellen Solution Enabler Programm von',
   start_presse_headline: 'Aktuelle Pressemeldungen',
